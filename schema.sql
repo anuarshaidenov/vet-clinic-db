b4e86427481eeda0373303d7aed6321763558b5d
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-02-24 22:34:26 +0600

    Add data, queries and schemas

--- schema.sql
+++ schema.sql
@@ -6,7 +6,29 @@ CREATE TABLE animals (
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
-    weight_kg decimal
+    weight_kg decimal,
+    PRIMARY KEY (id)
 );
 
 ALTER TABLE animals ADD species varchar(100);
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(255),
+    age INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE animals ADD PRIMARY KEY (id);
+
+ALTER TABLE animals DROP species;
+
+ALTER TABLE animals ADD species_id INT, ADD CONSTRAINT FK_SPECIES FOREIGN KEY(species_id) REFERENCES species (id);
+
+ALTER TABLE animals ADD owners_id INT, ADD CONSTRAINT FK_OWNERS FOREIGN KEY (owners_id) REFERENCES owners (id);
